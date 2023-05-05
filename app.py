09be385705c89f69d543e299767a4ca6214fcc11
Audrius Lietuvninkas <audrius.alt@gmail.com>
2023-05-05 17:53:48 +0300

    next.....

--- app.py
+++ app.py
@@ -1,64 +1,59 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from beem.account import Account
 from datetime import datetime, timedelta
 import requests
 import json
 from bchain import get_card_info
 
-
 app = Flask(__name__)
+MAX_DAYS = 90
+DEFAULT_DAYS = 30
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # Fetch the HTML data from the page
-    url = 'https://api.splinterlands.io/transactions/lookup'
-    acc = Account("altas")
+    if request.method == 'POST':
+        url = 'https://api.splinterlands.io/transactions/lookup'
+        card_lookup_url = 'https://api.splinterlands.io/cards/find'
+        username = request.form['username']
+        try:
+            days = int(request.form['days'])
+        except ValueError:
+            days = DEFAULT_DAYS
+        if days > MAX_DAYS:
+            days = MAX_DAYS
+        acc = Account(username)
+        stop = datetime.utcnow() - timedelta(days=days)
+        rows = []
+        for tx in acc.history_reverse(stop=stop):
+            if "id" in tx:
+                if tx["id"] == "sm_market_purchase":
+                    params = {
+                        'trx_id': tx["trx_id"]
+                    }
+                    response = requests.get(url, params=params).json()
+                    result_string = response['trx_info']['result']
 
-    # Parse the HTML using BeautifulSoup
+                    result = json.loads(result_string)
+                    by_seller = result["by_seller"]
+                    for seller in by_seller:
+                        items = seller["items"]
+                        for item in items:
+                            card_number, price = get_card_info(item)
+                            card_number_params = {'ids': card_number}
+                            card_details_response = requests.get(card_lookup_url, params=card_number_params).json()
+                            rate = result['total_usd'] / result['total_dec']
+                            rows.append({
+                                'date': datetime.strptime(response['trx_info']['created_date'], "%Y-%m-%dT%H:%M:%S.000Z"),
+                                'type': card_details_response[0]["details"]["name"],
+                                'card': card_number,
+                                'quantity': seller['seller'],
+                                'price_usd': f"$ {price:,.3f}",
+                                'price_dec': f"{(price / rate):,.3f} DEC",
+                                'rate': f"{rate:.6f}"
+                            })
 
-    # Find the table element containing the data
-
-
-    # Extract the data from the table
-
-    stop = datetime.utcnow() - timedelta(days=30)
-    rows = []
-    for tx in acc.history_reverse(stop=stop):
-        if "id" in tx:
-            if tx["id"] == "sm_market_purchase":
-                params = {
-                    'trx_id': tx["trx_id"]
-                }
-                response = requests.get(url, params=params).json()
-                result_string = response['trx_info']['result']
-
-                result = json.loads(result_string)
-                by_seller = result["by_seller"]
-                for seller in by_seller:
-                    items = seller["items"]
-                    for item in items:
-                        print('********************')
-                        print(result_string)
-                        print('********************')
-                        card_number, price = get_card_info(item)
-                        rate = result['total_usd'] / result['total_dec']
-                        rows.append({
-                            'date': response['trx_info']['created_date'],
-                            'type': response['trx_info']['type'],
-                            'card': card_number,
-                            'quantity': seller['seller'],
-                            'price_usd': price,
-                            'price_dec': price / rate
-                        })
-
-
-
-
-
-
-
-
-    # Render the template with the data
-    return render_template('index.html', rows=rows)
-
+        # Render the template with the data
+        return render_template('history.html', rows=rows, username=username, days=days)
+    else:
+        return render_template('index.html')
--- bchain.py
+++ bchain.py
@@ -4,13 +4,28 @@ import json
 
 def get_card_info(txid_index):
     tx_id = txid_index.split('-')
-    index = int(tx_id[1])
+    index = int(tx_id[-1])
+    op = 0
+    if len(tx_id) > 2:
+        op = int(tx_id[1])
     b = Blockchain()
     trans = b.get_transaction(tx_id[0])
-    json_string = trans['operations'][0]['value']['json']
+    json_string = trans['operations'][op]['value']['json']
     cards_info = json.loads(json_string)
 
     if isinstance(cards_info, dict):
-        return get_card_info(cards_info['ids'][0])
+        try:
+            card_id, old_price = get_card_info(cards_info['ids'][index])
+            new_price = cards_info['new_price']
+            return card_id, new_price
+        except KeyError:
+            card_id, old_price = get_card_info(cards_info['trx_ids'][index])
+            new_price = cards_info['new_price']
+            return card_id, new_price
 
-    return cards_info[0]['cards'][0], cards_info[0]['price']
+    try:
+        price = float(cards_info[index]['price'])
+    except ValueError:
+        price = 0.0
+
+    return cards_info[index]['cards'][0], price
